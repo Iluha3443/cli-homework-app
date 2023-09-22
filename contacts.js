4cd7b6d7501c7094f4325b7f128d9c21c33e552a
Iluha3443 <tkacenkoila01@gmail.com>
2023-09-22 23:39:03 +0300

    contacts

--- contacts.js
+++
@@ -1,3 +0,0 @@
-const data = [1, 2, 3]
-
-module.exports = data
--- index.js
+++ index.js
@@ -1,2 +1,31 @@
-const data = require("./contacts")
-console.log(data)
+import * as ContactsService from "./db/contacts.js"
+import yargs from "yargs"
+
+const invokeAction = async({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'list':
+          const contacts = await ContactsService.listContacts();
+          console.log(contacts)
+      break;
+
+    case 'get':
+      const contactsId = await ContactsService.getContactById(id);
+          console.log(contactsId)
+      break;
+
+    case 'add':
+     const addContact = await ContactsService.addContact(name, email, phone);
+          console.log(addContact)
+      break;
+
+    case 'remove':
+       const removeContact = await ContactsService.removeContact(id);
+          console.log(removeContact)
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction({action: "remove",id: "VGN-kGiGwNzi0ookCKrTr"})

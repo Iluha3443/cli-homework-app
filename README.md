c8d117b1fc2f693a763df06a2d0691f3eaf8a725
Iluha3443 <tkacenkoila01@gmail.com>
2023-09-23 19:57:14 +0300

    node

---
+++ README.md
@@ -0,0 +1,4 @@
+https://monosnap.com/file/KNMbkWjO6fz495SjosV9WmZ9q5xqdg //list
+https://monosnap.com/file/IPxathfAWVzbD9sQhF43u9LsQzRhn4 //getbyid
+https://monosnap.com/file/qHUC3gMSnM0S3ilYaLLdbKxZSMQqwv //add
+https://monosnap.com/file/EmZBXPvlwiBTLwwpHhVDVVvy83FHne //remove
--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 import * as ContactsService from "./db/contacts.js"
-import yargs from "yargs"
+import { program } from "commander";
 
-const invokeAction = async({ action, id, name, email, phone }) => {
+const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case 'list':
-          const contacts = await ContactsService.listContacts();
-          console.log(contacts)
+      const contacts = await ContactsService.listContacts();
+      console.log(contacts)
       break;
 
     case 'get':
       const contactsId = await ContactsService.getContactById(id);
-          console.log(contactsId)
+      console.log(contactsId)
       break;
 
     case 'add':
-     const addContact = await ContactsService.addContact(name, email, phone);
-          console.log(addContact)
+      const addContact = await ContactsService.addContact(name, email, phone);
+      console.log(addContact)
       break;
 
     case 'remove':
-       const removeContact = await ContactsService.removeContact(id);
-          console.log(removeContact)
+      const removeContact = await ContactsService.removeContact(id);
+      console.log(removeContact)
       break;
 
     default:
       console.warn('\x1B[31m Unknown action type!');
   }
-}
+};
 
-invokeAction({action: "remove",id: "VGN-kGiGwNzi0ookCKrTr"})
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+invokeAction(argv);
